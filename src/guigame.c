b879255aed35c8f6ce45ba36990a528e4799bd9b
峰哥 <362053534@qq.com>
2025-08-09 00:53:40 +0800

    Revert "test"
    This reverts commit 3822653306234ce6b8d81d91b4de6a5dea061980.

--- src/guigame.c
+++ src/guigame.c
@@ -978,7 +978,7 @@ static void guiGameSetDMASettingsState(void)
 
 static int guiGameDMAUpdater(int modified)
 {
-    if (true) {
+    if (modified) {
         guiGameSetDMASettingsState();
     }
 
