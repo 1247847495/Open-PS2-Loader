d86ea34aead4ed5ac7b2ef8aa06d9c4e0db67d8e
峰哥 <362053534@qq.com>
2025-08-17 01:55:26 +0800

    debug

--- src/texcache.c
+++ src/texcache.c
@@ -183,7 +183,7 @@ GSTEXTURE *cacheGetTexture(image_cache_t *cache, item_list_t *list, int *cacheId
             if (!buttonPressedOnce) {
                 buttonPressedOnce = 1;
                 cdFramesCount = 1;
-                cdFrames = 50;
+                cdFrames = 1000;
             }
         } else
             buttonPressedOnce = 0;
