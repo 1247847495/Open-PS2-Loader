36946033c8fd85a025d8189a002306ffab557516
峰哥 <362053534@qq.com>
2025-08-18 14:48:53 +0800

    尝试优化代码

--- src/texcache.c
+++ src/texcache.c
@@ -61,10 +61,10 @@ static void cacheLoadImage(void *data)
 
     // 光标指向的游戏ID和后台加载的art图片不符时，或者已经处于CD(按住和快速点击)时，停止加载图片，避免卡顿
     if ((curStartUp && strncmp(curStartUp, req->value, 11)) || (gScrollSpeed > 0 ? isRepeating : 0) || cdFramesCount) {
-        req->entry->lastUsed = 0;
-        req->entry->UID = -1;
-        //req->cacheUID = -1;
-        req->entry->qr = NULL;
+        //req->entry->lastUsed = -1;
+        //req->entry->UID = 0;
+        //req->entry->qr = NULL;
+        cacheClearItem(req->entry, 1);
         free(req);
         return;
     }
