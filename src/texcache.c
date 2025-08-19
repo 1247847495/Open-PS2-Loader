9f66047cb48437795c57eb2e792fd55d944d5e61
峰哥 <362053534@qq.com>
2025-08-19 17:26:23 +0800

    使用getKeyPressed方法替代guiInactiveFrames，使按键判定更严谨

--- src/texcache.c
+++ src/texcache.c
@@ -160,7 +160,7 @@ GSTEXTURE *cacheGetTexture(image_cache_t *cache, item_list_t *list, int *cacheId
 
     // 默认情况下，触发重复按键时，就会跳过所有Qr
     if (isRepeating) {
-        if (guiInactiveFrames)
+        if (!getKeyPressed(KEY_UP) && !getKeyPressed(KEY_DOWN) && !getKeyPressed(KEY_L1) && !getKeyPressed(KEY_R1))
             isRepeating = 0;
         findBGCount = 0;
         cdFramesCount = 0; // 强制结束连按CD
@@ -181,7 +181,7 @@ GSTEXTURE *cacheGetTexture(image_cache_t *cache, item_list_t *list, int *cacheId
         }
 
         // 连按CD期间，再次按键，重置帧数
-        if (!guiInactiveFrames) {
+        if (getKeyPressed(KEY_UP) || getKeyPressed(KEY_DOWN) || getKeyPressed(KEY_L1) || getKeyPressed(KEY_R1)) {
             cdFramesCount = 1;
             // 按下按键只重置一次的变量
             if (!buttonPressedOnce) {
