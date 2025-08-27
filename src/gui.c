77d9fe0f061b429392376e0c848f5e4b843c6acb
峰哥 <362053534@qq.com>
2025-08-27 15:41:50 +0800

    尝试解决欢迎界面闪烁问题

--- src/gui.c
+++ src/gui.c
@@ -1571,7 +1571,7 @@ void guiIntroLoop(void)
     while (!endIntro) {
         guiStartFrame();
 
-        //guiShow();
+        guiShow(); // 可以防止同时开启USB和HDD时，欢迎界面会闪烁的问题（光标停留在USB第一个游戏仍会闪烁）
 
         guiRenderGreeting(0x80);
 
