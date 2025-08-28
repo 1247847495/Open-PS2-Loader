d96e4e8531a5da2af3c8f27270d7c83feb054ab4
峰哥 <362053534@qq.com>
2025-08-28 20:28:19 +0800

    修复编译报错

--- include/gui.h
+++ include/gui.h
@@ -60,7 +60,7 @@ extern int MX4SIOFound;
 extern int GptFound;
 extern int txtFileCreated;
 extern int txtFileRebuilded;
-extern int BdmStarted = 0; // BDM模式已启动
+extern int BdmStarted; // BDM模式已启动
 extern int busyAlpha; // Fully transparant
 
 #define GUI_SCREEN_MAIN      0
