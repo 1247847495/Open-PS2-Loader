cb5b46a1b9f83be374c6fd2d009183a4b844d63f
峰哥 <362053534@qq.com>
2025-09-06 01:13:25 +0800

    debug

--- src/texcache.c
+++ src/texcache.c
@@ -355,7 +355,7 @@ GSTEXTURE *cacheGetTexture(image_cache_t *cache, item_list_t *list, int *cacheId
         strcpy(debugFileDir, "smb:debug-TexCacheCacheId.txt");
         FILE *debugFile = fopen(debugFileDir, "ab+");
         if (debugFile != NULL) {
-            fprintf(debugFile, "cacheId:%d   entry1->UID:%d   *UID:%d    lastUsed:%d    %s\r\n\r\n", *cacheId, entry1->UID, *UID, entry->lastUsed, cache->suffix);
+            fprintf(debugFile, "cacheId:%d   entry1->UID:%d   *UID:%d    lastUsed:%d    %s\r\n\r\n", *cacheId, entry1->UID, *UID, entry1->lastUsed, cache->suffix);
             fclose(debugFile);
         }
     }
