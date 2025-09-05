8e1fe09992ae1de599d6414f97f30df02faed19c
峰哥 <362053534@qq.com>
2025-09-05 20:11:02 +0800

    debug

--- src/texcache.c
+++ src/texcache.c
@@ -355,6 +355,18 @@ GSTEXTURE *cacheGetTexture(image_cache_t *cache, item_list_t *list, int *cacheId
         }
     }
 
+    cache_entry_t *entry1 = &cache->content[*cacheId];
+    if (entry1->UID == 2) {
+        // debug  打印debug信息
+        char debugFileDir[64];
+        strcpy(debugFileDir, "smb:debug-TexCacheCacheId.txt");
+        FILE *debugFile = fopen(debugFileDir, "ab+");
+        if (debugFile != NULL) {
+            fprintf(debugFile, "cacheId:%d   entry1->UID:%d   *UID:%d \r\n\r\n", *cacheId, entry1->UID, *UID);
+            fclose(debugFile);
+        }
+    }
+
     // -2代表无图像，-1代表正在查找图像，0-9代表缓存编号
     if (*cacheId == -2) {
         // 根据图像类型，将缓存分类保存，替代NULL时的默认图(防止闪烁)
@@ -368,14 +380,6 @@ GSTEXTURE *cacheGetTexture(image_cache_t *cache, item_list_t *list, int *cacheId
         return NULL;
     } else if (*cacheId != -1) {
         cache_entry_t *entry = &cache->content[*cacheId];
-        // debug  打印debug信息
-        char debugFileDir[64];
-        strcpy(debugFileDir, "smb:debug-TexCacheCacheId.txt");
-        FILE *debugFile = fopen(debugFileDir, "ab+");
-        if (debugFile != NULL) {
-            fprintf(debugFile, "cacheId:%d   entry->UID:%d   *UID:%d \r\n\r\n", *cacheId, entry->UID, *UID);
-            fclose(debugFile);
-        }
         if (entry->UID == *UID) {
             if (entry->qr) {
                 return prevCache ? prevCache : NULL;
