bbe3ef1fcf7d39413edcb9555e916ac4bf2c1147
峰哥 <362053534@qq.com>
2025-09-12 17:02:25 +0800

    测试不加UnloadTex是否能解决随机卡死？

--- src/textures.c
+++ src/textures.c
@@ -255,7 +255,6 @@ static int texSizeValidate(int width, int height, u8 psm)
 
 static void texPrepare(GSTEXTURE *texture)
 {
-    texFree(texture);
     texture->Width = 0;                              // Must be set by loader
     texture->Height = 0;                             // Must be set by loader
     texture->PSM = GS_PSM_CT24;                      // Must be set by loader
