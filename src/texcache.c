30c8fa421f791cbfd67a53df932c06653f22f043
峰哥 <362053534@qq.com>
2025-09-14 01:16:27 +0800

    Revert "test3"
    This reverts commit 4f28e275e8a8fcab1e9c6551ecd90e1918dc6326.

--- src/texcache.c
+++ src/texcache.c
@@ -170,6 +170,8 @@ static void cacheLoadImage(void *data)
             req->entry->lastUsed = guiFrameId;
             req->entry->texReady = 1;
         }
+        while (1) {
+        }
         req->entry->qr = NULL;
         free(req);
         batchRequests[i] = NULL; // 及时清理，避免野指针
@@ -232,7 +234,7 @@ GSTEXTURE *cacheGetTexture(image_cache_t *cache, item_list_t *list, int *cacheId
     if (curStartUp != value) {
         // 移动光标时，如果有IO请求，就会跳过Qr，后台也会停止继续加载队列中的图片
         if (curStartUp && !padGetRepeating() && !ForceRefreshPrevTexCache && ioQuesting)
-            ioRemoveRequests(IO_CACHE_LOAD_ART);
+            cdFramesCount = 1; // 触发连按CD
         curStartUp = value;
     }
 
