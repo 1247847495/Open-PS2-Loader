cd9e101f54c6c31496d9827ce24660e47e63e4c3
峰哥 <362053534@qq.com>
2025-09-15 22:00:26 +0800

    test

--- src/texcache.c
+++ src/texcache.c
@@ -339,7 +339,7 @@ GSTEXTURE *cacheGetTexture(image_cache_t *cache, item_list_t *list, int *cacheId
         if (entry->UID == *UID) {
             if (entry->qr) {
                 return PrevCacheID < 0 ? NULL : &cache->content[PrevCacheID].texture;
-            } else if (entry->lastUsed == 0) {
+            } else if (entry->texFound == 0) {
                 *cacheId = -2;
                 // 根据图像类型，将缓存分类保存，替代NULL时的默认图(防止闪烁)
                 if (!strncmp("COV", cache->suffix, 3))
@@ -379,7 +379,7 @@ GSTEXTURE *cacheGetTexture(image_cache_t *cache, item_list_t *list, int *cacheId
         *cacheId = -1;
     }
 
-    if (skipQr || texLoading)
+    if (skipQr || texLoading || entry->lastUsed == 0)
         return PrevCacheID < 0 ? NULL : &cache->content[PrevCacheID].texture;
 
     cache_entry_t *currEntry, *oldestEntry = NULL;
