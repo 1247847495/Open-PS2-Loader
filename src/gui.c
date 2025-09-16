0c29fbfdd6491593e22bd8848689f4a0ddc123b9
峰哥 <362053534@qq.com>
2025-09-16 22:42:44 +0800

    自动连按和图片加载机制修改

--- src/gui.c
+++ src/gui.c
@@ -1575,8 +1575,8 @@ void guiUpdateScrollSpeed(void)
     // fast - 100 ms
     // medium - 300 ms
     // slow - 500 ms
-    setButtonDelay(KEY_UP, 500 - gScrollSpeed * 200); // 0,1,2 -> 500, 300, 100
-    setButtonDelay(KEY_DOWN, 500 - gScrollSpeed * 200);
+    setButtonDelay(KEY_UP, 40 - gScrollSpeed * 15); // 0,1,2 -> 40, 25, 10
+    setButtonDelay(KEY_DOWN, 40 - gScrollSpeed * 15);
 }
 
 void guiUpdateScreenScale(void)
