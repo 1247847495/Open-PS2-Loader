a313459c15f482cd3a8f0b1a30a8405c6c24e75b
峰哥 <362053534@qq.com>
2025-09-22 21:47:17 +0800

    Reapply "防止自动模式，初始化时，数据不同步问题"
    This reverts commit c71b6a212423ba79dddff29ce76a5d90e3d678da.

--- src/opl.c
+++ src/opl.c
@@ -496,7 +496,8 @@ void initSupport(item_list_t *itemList, int mode, int force_reinit)
         if (((force_reinit) && (mod->support->enabled)) || (startMode == START_MODE_AUTO && !mod->support->enabled)) {
             mod->support->itemInit(mod->support);
             moduleUpdateMenuInternal(mod, 0, 0);
-            ioPutRequest(IO_MENU_UPDATE_DEFFERED, &list_support[mode].support->mode); // can't use mode as the variable will die at end of execution
+            // ioPutRequest(IO_MENU_UPDATE_DEFFERED, &list_support[mode].support->mode); // can't use mode as the variable will die at end of execution
+            menuDeferredUpdate(&list_support[mode].support->mode); // 使用单线程，防止初始化时，数据不同步问题。
         }
     } else {
         // If the module has a valid menu instance try to refresh the visibility state.
