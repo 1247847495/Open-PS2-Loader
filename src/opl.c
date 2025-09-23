bcb76822938e8e5abe23008e4ff3bf4206a71209
峰哥 <362053534@qq.com>
2025-09-23 21:59:23 +0800

    Revert "test"
    This reverts commit 66992f6a184c93415216818f3261b424e297c89c.

--- src/opl.c
+++ src/opl.c
@@ -1295,11 +1295,6 @@ static void loadSupportsBackground(void)
 
         moduleUpdateMenuInternal(&list_support[i], changed_backLoad, langChanged_backLoad);
     }
-    if (firstOpenOPL) {
-        firstOpenOPL = 0;
-        deferredAudioInit();
-        deferredInit();
-    }
 }
 void applyConfig(int themeID, int langID, int skipDeviceRefresh)
 {
