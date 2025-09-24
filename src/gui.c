3d5d23933f34488e6b47992b9ca8a516522810e8
峰哥 <362053534@qq.com>
2025-09-25 07:21:19 +0800

    test7

--- src/gui.c
+++ src/gui.c
@@ -1436,7 +1436,7 @@ static int busyAlpha = 0x00; // Fully transparant
 static void guiDrawOverlays()
 {
     // are there any pending operations?
-    int pending = (ioHasPendingRequests() || !mainScreenInitDone || texLoading);
+    int pending = (ioHasPendingRequests());
 
     if (!pending) {
         // Fade out
@@ -1806,8 +1806,8 @@ void guiMainLoop(void)
             }
         }
 
-        if (greetingAlpha > 0x00)
-            guiRenderGreeting(greetingAlpha);
+        //if (greetingAlpha > 0x00)
+        //    guiRenderGreeting(greetingAlpha);
 
         // Render overlaying gui thingies :)
         guiDrawOverlays();
