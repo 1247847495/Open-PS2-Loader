3ec9e2237d3197ac3d6d601e1451052fd2d68d04
峰哥 <362053534@qq.com>
2025-09-25 08:30:56 +0800

    添加signal信号结束线程

--- src/texcache.c
+++ src/texcache.c
@@ -178,6 +178,7 @@ void cacheEnd()
 {
     // nothing to do... others have to destroy the cache via cacheDestroyCache
     //forceSkipQr = 1;
+    pthread_cond_signal(&cond);
     pthread_join(tid, NULL); // 等待线程结束
 }
 
