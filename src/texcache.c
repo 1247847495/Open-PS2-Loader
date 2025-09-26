25cbd662f0972579edfcca72402b79fce787f749
峰哥 <362053534@qq.com>
2025-09-26 15:49:47 +0800

    修复只读一张图的问题

--- src/texcache.c
+++ src/texcache.c
@@ -214,9 +214,8 @@ GSTEXTURE *cacheGetTexture(image_cache_t *cache, item_list_t *list, int *cacheId
     // 启动id变化时，说明光标有移动（可能用UID判断，效率更高更合理，之后再改。UID一开始是-1，然后再分配一个正整数）
     if (curStartUp != value) {
         // 移动光标时，如果有IO请求，就会跳过Qr，后台也会停止继续加载队列中的图片
-        if (curStartUp && !padGetRepeating() && !ForceRefreshPrevTexCache)
-            if (!padGetRepeating())
-                cdFramesCount = 1; // 触发连按CD
+        if (curStartUp && !padGetRepeating() && !ForceRefreshPrevTexCache && ioHasPendingRequests())
+            cdFramesCount = 1; // 触发连按CD
         curStartUp = value;
     }
 
