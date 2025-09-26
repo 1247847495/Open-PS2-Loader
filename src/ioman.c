66471f9f3c92df3ca57309a9234e3090729d0be0
峰哥 <362053534@qq.com>
2025-09-26 22:49:02 +0800

    尝试解决ioEnd卡住问题1

--- src/ioman.c
+++ src/ioman.c
@@ -185,8 +185,6 @@ static void ioWorkerThread(void *arg)
     isIOPending = 0;
     isIORunning = 0;
     SignalSema(gEndSemaId);
-
-    ExitDeleteThread();
 }
 
 static void ioSimpleActionHandler(void *data)
@@ -319,9 +317,14 @@ void ioEnd(void)
     gIOTerminate = 1;
     WakeupThread(gIOThreadId);
 
+    // 等待worker线程彻底退出
+    while (isIORunning)
+        usleep(1000); // 或者YieldCPU(), 可以根据PS2线程API适当替换
+
     // 此时信号量一定没人再用，可以销毁
     DeleteSema(gEndSemaId);
     DeleteSema(gIOPrintfSemaId);
+    ExitDeleteThread();
 }
 
 int ioGetPendingRequestCount(void)
@@ -383,7 +386,7 @@ int ioBlockOps(int block)
 
         // wait for all io to finish
         while (ioHasPendingRequests())
-            sleep(0);
+            usleep(1000);
 
         ChangeThreadPriority(ThreadID, status.current_priority);
 
