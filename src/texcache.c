f1f4054917a4188553155fbc161c6dcbc3b71b04
峰哥 <362053534@qq.com>
2025-09-29 23:22:23 +0800

    test新的加载图片方法

--- src/texcache.c
+++ src/texcache.c
@@ -433,6 +433,7 @@ GSTEXTURE *cacheGetTexture(image_cache_t *cache, item_list_t *list, int *cacheId
     //        *cacheId = i;
     //    }
     //}
+    *cacheId = 0;
     cache_entry_t *currEntry = &cache->content[*cacheId];
     if (currEntry->texFound == -1 && !currEntry->qr) {
         cacheClearItem(currEntry, 1);
