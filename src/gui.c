d1bb4da9c87bd80df3779e632de62047a6a7d447
峰哥 <362053534@qq.com>
2025-10-06 06:20:12 +0800

    优化代码

--- src/gui.c
+++ src/gui.c
@@ -1835,7 +1835,7 @@ void guiMainLoop(void)
             if (artLoadDelayTime > 0) {
                 artLoadDelayTime--;
                 // 启动画面的延迟期间，预加载art图片
-                if (texLoading == 0)
+                if (!texLoading)
                     artLoadDelayTime = 0;
                 if (artLoadDelayTime <= 0) {
                     // 手动启动BDM后的变量处理
